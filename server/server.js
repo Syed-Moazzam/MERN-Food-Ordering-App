363f5e8bff8f74ff93e96d7abec4fa82029b2238
Syed-Moazzam <moazzamahmed05@gmail.com>
2024-09-09 04:13:09 +0500

    deploying application

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import path from 'path';
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
 import foodRouter from "./routes/foodRoute.js";
@@ -12,12 +13,31 @@ import emailRouter from "./routes/emailRoute.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
+const __dirname = path.resolve();
+
 // middlewares
 app.use(express.json());
 app.use(cors());
 
 // db connection
 connectDB();
+
+// Serve client build files
+app.use('/client', express.static(path.join(__dirname, '../client/dist')));
+
+// Serve admin build files
+app.use('/admin', express.static(path.join(__dirname, '../admin/dist')));
+
+// Fallback to the client app for any route not caught by the server
+app.get('/client/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
+// Fallback to the admin app for /admin routes
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../admin/dist', 'index.html'));
+});
+
 
 // api endpoints
 app.use("/api/user", userRouter);
